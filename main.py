048c70c7e1a8c65f4b3c9b053d2ec24f21c420a9
sats1500 <satishsharma1500@gmail.com>
2018-03-14 19:10:17 +0100

    change

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ import matplotlib.pyplot as plt
 
 
 Node = dt.bus.shape[0]
-print(Node)
-for n in range(Node)
-    n1 = NodeAgent('N'+str(1), 'node', 0.14, 14, 0, 2.0, 0.0, 0.0)
-n2 = NodeAgent('N'+str(2), 'node', 0.15, 15, 0, 2.5, 3.0, 0.9861)
+print('node = '+str(Node))
+
+allagents = []
+for n in range(Node):
+    tagent = NodeAgent('N'+str(1), 'node', dt.gencost.item((n,4)), dt.gencost.item((n,5)), dt.gen.item((n,8)), 2.0, 0.0, 0.0)
+    allagents.append(tagent)
+
+
+print allagents[0].a

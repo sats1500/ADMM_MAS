156a630c999933d36d4e7d79344ba7f855ab4b2a
sats1500 <satishsharma1500@gmail.com>
2018-03-19 17:18:33 +0100

    change 19march18

--- agent.py
+++ agent.py
@@ -1,18 +1,10 @@
 import numpy as np
 
 class NodeAgent(object):
-    def __init__(self, id, num, a, b, Pmin, Pmax, Pd, Qd, neig):
+    def __init__(self, id, bus):
         self.id = id
-        self.type = num
-        self.a = a  # variable term in bid curve bc = 2aP + b
-        self.b = b  # constant term in bid curve bc = 2aP + b
-        self.Pmin = Pmin
-        self.Pmax = Pmax
-        self.Pg = 0.0
-        self.Qg = 0.0
-        self.Pd = Pd
-        self.Qd = Qd
-        self.neig = neig
-        self.value = 0.0
+        self.bus = bus
+        self.neighbors = dict()
 
-    def connection(self,branch):
+    def connection(self, branch):
+        pass
--- main.py
+++ main.py
@@ -1,16 +1,33 @@
 import numpy as np
 
 from agent import NodeAgent
-import case as dt
+import case14 as dt
 import matplotlib.pyplot as plt
 
+case = dt.case14()
 
-Node = dt.bus.shape[0]
-print('node = '+str(Node))
+agents = dict()
+branches = dict()
 
-allagents = []
-for n in range(Node):
-    tagent = NodeAgent('N'+str(1), n, dt.gencost.item((n,4)), dt.gencost.item((n,5)), dt.gen.item((n,8)), 2.0, 0.0, 0.0)
-    allagents.append(tagent)
+for item in case["bus"]:
+    agents[int(item[0])] = NodeAgent(int(item[0]), item[1:])
+
+
+for item in case["branch"]:
+
+    node_f = int(item[0])
+    node_t = int(item[1])
+
+    if node_f not in agents[node_t].neighbors:
+        agents[node_t].neighbors[node_f] = agents[node_f]
+
+    if node_t not in agents[node_f].neighbors:
+        agents[node_f].neighbors[node_t] = agents[node_t]
+
+for agent in agents.values():
+    print agent.id, agent.neighbors
+
+# for ag in agents.values():
+#     print ag.id, ag.bus
 
 
